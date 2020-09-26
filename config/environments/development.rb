a153acb4d7b52916f10e0374d46ddad7d11e810b
KeisukeKawakita <kkawaki@gmail.com>
2020-09-26 19:32:58 +0900

    mail7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    address: "smtp.gmail.com",
+    address: "smtp.gmail",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
